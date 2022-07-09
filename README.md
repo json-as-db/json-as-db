2d75c906dccb8c945044db63ecded1d63a71ecb6
siCasta <sicastaprojectos@gmail.com>
2022-07-09 12:38:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# JSON as db
+
+Una base de datos en formato JSON.
+
+[![NPM Version][npm-version-image]][npm-version-url]
+[![NPM Install Size][npm-install-size-image]][npm-install-size-url]
+[![NPM License][license-image]][license-url]
+
+```js
+import db from 'json-as-db'
+
+const Products = new db({
+	filePath: './src/data/products.json',
+	tableName: 'products',
+})
+```
+
+## Instalación
+
+```console
+$ npm i json-as-db
+```
+
+## Uso
+
+Es facil solo tienes que crear el archivo json y poner las tablas que desees recuerda que si no existe la tabla llamada en la clase dara un error no te preocupes por un id esta ya lo genera por ti.
+
+```json
+{
+	"products": {
+		"uuid": {
+			"name": "",
+			"price": 0,
+			"description": ""
+		}
+	}
+}
+```
+
+```js
+const Products = new db('./src/db/database.json', 'products')
+
+async function getProducts() {
+	return await Products.get()
+}
+```
+
+## Metodos
+
+Estos son lo metodos que tiene la clase
+
+-   get()
+-   getById()
+-   create()
+-   update()
+-   delete()
+
+[npm-version-url]: https://www.npmjs.com/package/json-as-db
+[npm-version-image]: https://badgen.net/npm/v/json-as-db
+[npm-install-size-image]: https://packagephobia.com/badge?p=json-as-db
+[npm-install-size-url]: https://packagephobia.com/result?p=json-as-db
+[license-image]: https://badgen.net/npm/license/json-as-db
+[license-url]: https://github.com/siCasta/db-json/blob/main/LICENSE
---
+++ src/index.js
@@ -0,0 +1,83 @@
+const { promises, existsSync } = require('fs')
+const { v4: uuidv4 } = require('uuid')
+
+function JAD({ filePath, tableName }) {
+    if (!filePath && !tableName) {
+        throw new Error('filePath and tableName are required')
+    }
+
+    const getData = async () => {
+        try {
+            if (!existsSync(filePath)) await promises.writeFile(filePath, '{}')
+
+            const jsondata = await promises.readFile(filePath, 'utf8')
+            const data = JSON.parse(jsondata)
+
+            if (!data[tableName]) data[tableName] = {}
+
+            return data
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+
+    const create = async (object) => {
+        try {
+            const data = await getData()
+            const id = uuidv4()
+            data[tableName][id] = object
+            await promises.writeFile(filePath, JSON.stringify(data))
+            return id
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+
+    const get = async () => {
+        try {
+            const data = await getData()
+            return data[tableName]
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+
+    const getById = async (id) => {
+        try {
+            const data = await getData()
+            return data[tableName][id]
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+
+    const update = async (id, object) => {
+        try {
+            const data = await getData()
+            data[tableName][id] = object
+            await promises.writeFile(filePath, JSON.stringify(data))
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+
+    const deleteById = async (id) => {
+        try {
+            const data = await getData()
+            delete data[tableName][id]
+            await promises.writeFile(filePath, JSON.stringify(data))
+        } catch (err) {
+            throw new Error(err)
+        }
+    }
+
+    return {
+        create,
+        get,
+        getById,
+        update,
+        deleteById
+    }
+}
+
+module.exports = JAD
